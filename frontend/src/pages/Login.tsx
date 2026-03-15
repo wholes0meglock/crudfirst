b74145e3a2574fd5787d06cf16d7e2e5b097ba25
glock <glockman2309@gmail.com>
2026-03-15 19:14:50 +0530

    .

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -6,15 +6,43 @@ function userLogin()
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
-    async function handleSubmit(e:)
+    async function handleSubmit(e : React.FormEvent)
     {
         e.preventDefault();
         try
         {
             const data = await login(username,password);
-
-
+            navigate("/dashboard");
+        }
+        catch(error)
+        {
+            console.error("Login failed ", error);
         }
 
     }
+    return (
+        <div>
+            <h2>Login</h2>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
+
+export default userLogin;
