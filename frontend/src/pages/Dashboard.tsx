7c7959ebfe800aaa86aad3f95f45e221e3f6753d
glock <glockman2309@gmail.com>
2026-03-19 02:25:41 +0530

    dash

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom"
+import { getSessions } from "../services/api";
+type Session = {
+  _id: string;
+  subject: string;
+  duration: number;
+};
+function Dashboard()
+{
+    const [sessions,setSessions] = useState<Session[]>([]); //useState<Type>(initialValue)
+    useEffect(() =>
+    {
+        async function loadSessions()
+        {
+            try
+            {
+                const data = await getSessions();
+                setSessions(data);
+            }
+            catch(e)
+            {
+                console.error("Error loading sessions", e);
+            }
+        }
+        loadSessions();
+    },[]);
+
+    return (
+        <div>
+            <h2> Dashboard </h2>
+            {sessions.map(session => (
+              <div key={session._id}>
+                  {session.subject} - {session.duration}
+              </div>
+          ))}
+        </div>
+    );
+}
+
+export default Dashboard;
